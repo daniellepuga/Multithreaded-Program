36d2302ec8cdf248d0d23938ca9a5a25cc321ac7
Danielle Puga <55933390+daniellepuga@users.noreply.github.com>
2023-04-04 12:38:41 -0700

    fixed spacing

--- hellothread.C
+++ hellothread.C
@@ -4,16 +4,13 @@
 #include <stdio.h>
 
 void *run(void *arg){
-  int iteration = 0;
+  int i = 0;
   char *string_to_print = arg;
-  while(iteration < 5) {
 
-    printf("%s%d\n", string_to_print, iteration);
-
-    iteration++;
-
+  while(i < 5) {
+    printf("%s%d\n", string_to_print, i);
+    i++;
   }
-  
   return NULL;
 }
 
@@ -22,11 +19,11 @@ char *thread_2 = "thread 2: ";
 
 int main() {
   printf("Launching threads\n");
+
   pthread_t t1, t2;
 
   pthread_create(&t1, NULL, run, thread_1);
   pthread_create(&t2, NULL, run, thread_2);
-
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
 
